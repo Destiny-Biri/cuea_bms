5666eb1570475ff043bb90867a6ad8772b15e68d
Albert Kamau <kamau.albert@gmail.com>
2020-09-06 03:03:10 +0300

    updated menu

--- admin/pages/reports.php
+++ admin/pages/reports.php
@@ -1,5 +1,5 @@
 <div class="grid-x grid-margin-x">
-	<div class="large-10 cell">
+	<div class="large-9 cell">
 		<h2>Reports</h2>
 		<?php
 		if(isset($_GET['report'])){
@@ -39,9 +39,9 @@
 ?>
 	</div>
 
-	<div class="large-2 cell">
+	<div class="large-3 cell">
 		<h4>Select Report Type</h4>
-		<ul class="no-bullet">
+		<ul class="menu vertical">
 			<li><a href="index.php?action=view&view=reports&report=journey">Sales By Journey</a></li>
 			<li><a href="index.php?action=view&view=reports&report=route">Sales By Route</a></li>
 			<li><a href="index.php?action=view&view=reports&report=dailysales">Daily Sales</a></li>
